362e8308c29742911015069c08b45029e665855c
Juan <50636966+juanfernandotirado@users.noreply.github.com>
2019-06-04 12:14:39 -0700

    Update RestaurantDetail.vue

---
+++ src/components/restaurant/RestaurantDetail.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="restaurant-detail">
+    <h1>Restaurant Detail</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'RestaurantDetail',
+    data:function(){
+    return{}
+    },
+  
+}
+</script>
