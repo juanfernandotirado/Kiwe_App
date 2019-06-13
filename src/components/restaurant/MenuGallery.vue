a790f17ce126c34fd44ecd139a61e17efe48fe24
Sunny Xue <sunny.sit26@gmail.com>
2019-06-13 10:58:17 -0700

    Install Axios

--- src/components/restaurant/MenuGallery.vue
+++ src/components/restaurant/MenuGallery.vue
@@ -19,7 +19,14 @@ export default {
 
   created(){
 
+    let api = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJZ04tZ9ZzhlQRDsJZp_cOS4U&key=AIzaSyCxKHIpSrggNO7p1N-n7V0FkJ8DohiK9MQ';
     
+    this.axios.get(api,{ crossdomain: true }).then((response) => {
+        console.log(response.data)
+      })
+
+ 
+
   }
     
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
 
 
 Vue.config.productionTip = false
