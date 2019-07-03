49fa57d55ef77a5f6756ad1be57c6be864c5521e
Sunny Xue <sunny.sit26@gmail.com>
2019-07-02 17:04:28 -0700

    Move scan function to HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -66,7 +66,47 @@ export default {
     },
 
     scanCode:function(){
-      this.$router.push('qrCode');
+      // this.$router.push('qrCode');
+
+      let that = this;
+        window.cordova.plugins.barcodeScanner.scan(
+              function (result) {
+                            // alert("We got a barcode\n" +
+                            //         "Result: " + result.text + "\n" +
+                            //         "Format: " + result.format + "\n" +
+                            //         "Cancelled: " + result.cancelled);
+
+                            let scanRid = result.text;
+                            let restList = that.$store.state.restaurantList;
+                            console.log(scanRid)
+                            console.log(restList);
+                            restList.map(item => {
+                                if(item.rid === scanRid){
+                                    that.$store.dispatch("assignRest", item);
+                                    that.$router.push('restDetail')
+                                }
+                            })
+
+                        },
+                        function (error) {
+                            alert("Scanning failed: " + error);
+                        },
+                        {
+                            preferFrontCamera : false, // iOS and Android
+                            showFlipCameraButton : true, // iOS and Android
+                            showTorchButton : true, // iOS and Android
+                            torchOn: true, // Android, launch with the torch switched on (if available)
+                            saveHistory: true, // Android, save scan history (default false)
+                            prompt : "Place a barcode inside the scan area", // Android
+                            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+                            formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+                            orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+                            disableAnimations : true, // iOS
+                            disableSuccessBeep: false // iOS and Android
+                        }
+                    );
+            
+
     }
   }
 }
--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -12,7 +12,6 @@
     <div class="buttons">
         <button class="btn" v-on:click="submitLogin" type="submit">Log in</button>
         <button class="btn" v-on:click="signUp">Sign up</button>
-        <button class="btn" @click="test">TEst</button>
     </div>
 
      <!-- <router-link to="/signup">Not on virtual line-up yet? Sign up</router-link> -->
@@ -84,46 +83,7 @@ export default {
                 this.errMsg = err.message;
               });
                     },
-         test:function(){
-                let that = this;
-                window.cordova.plugins.barcodeScanner.scan(
-                        function (result) {
-                            // alert("We got a barcode\n" +
-                            //         "Result: " + result.text + "\n" +
-                            //         "Format: " + result.format + "\n" +
-                            //         "Cancelled: " + result.cancelled);
-
-                            let scanRid = result.text;
-                            let restList = that.$store.state.restaurantList;
-                            alert(scanRid)
-                            console.log(scanRid)
-                            console.log(restList);
-                            restList.map(item => {
-                                if(item.rid === scanRid){
-                                    that.$store.dispatch("assignRest", item);
-                                    that.$router.push('restDetail')
-                                }
-                            })
-
-                        },
-                        function (error) {
-                            alert("Scanning failed: " + error);
-                        },
-                        {
-                            preferFrontCamera : false, // iOS and Android
-                            showFlipCameraButton : true, // iOS and Android
-                            showTorchButton : true, // iOS and Android
-                            torchOn: true, // Android, launch with the torch switched on (if available)
-                            saveHistory: true, // Android, save scan history (default false)
-                            prompt : "Place a barcode inside the scan area", // Android
-                            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-                            formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-                            orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-                            disableAnimations : true, // iOS
-                            disableSuccessBeep: false // iOS and Android
-                        }
-                    );
-            }
+        
                 }
             }
   </script>
